cde203a9f16afb25e3486a5129c6ba0419531c68
Andrew King <king1031@umn.edu>
2017-12-10 16:10:09 -0600

    Correcting foreign key from approach to event

--- sql/schema/create_table_approach.sql
+++ sql/schema/create_table_approach.sql
@@ -1,7 +1,7 @@
 CREATE TABLE ntsb_approach(
     runway_id VARCHAR(20),
     runway_airport_id VARCHAR(10),
-    event_number VARCHAR(30) REFERENCES ntsb_event(event_number),
+    event_number VARCHAR(30) REFERENCES ntsb_event(evt_number),
     ifr_approach_type VARCHAR(30),
     vfr_approach_type VARCHAR(30),
     PRIMARY KEY (runway_id,runway_airport_id,event_number),
