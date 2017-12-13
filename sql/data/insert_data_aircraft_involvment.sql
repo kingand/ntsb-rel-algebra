1681e5b63baa6d647f5dd445b9c28738376e205c
Andrew King <king1031@umn.edu>
2017-12-12 19:20:29 -0600

    Adding SQL insert scripts for a few tables

---
+++ sql/data/insert_data_aircraft_involvment.sql
@@ -0,0 +1,21 @@
+INSERT INTO ntsb_aircraft_category (
+    aircr_serial_num,
+    aircr_mfrr,
+    geo_location_lat_deg,
+    geo_location_lat_dir,
+    geo_location_lon_deg,
+    geo_location_lon_dir,
+    aircr_damage,
+    aircr_fire,
+    aircr_explosion
+) VALUES (
+    105-0395,
+    Hughes,
+    45.561,
+    N,
+    5.855,
+    W,
+    Substantial,
+    None,
+    None
+);
