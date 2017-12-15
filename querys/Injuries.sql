13ca9ee8b087d9dbb4cd35d8f0e48d72b0d365a9
Andrew King <king1031@umn.edu>
2017-12-15 03:07:25 -0600

    Fixing Injuries query

--- querys/Injuries.sql
+++ querys/Injuries.sql
@@ -1,43 +1,36 @@
 DECLARE
     BEGIN
         ops.go(
-            ops.times_ra('ntsb_engine_installation', 'ntsb_engine', 'Eng_EngInst_pair')
+            ops.times_ra('install=ntsb_engine_installation', 'eng=ntsb_engine', 'Eng_EngInst_pair')
         );
 
         ops.go(
             ops.filter_ra(
-                ops.filter_ra('Eng_EngInst_pair', 'ntsb_engine.eng_mfrr=ntsb_engine_installation.eng_mfrr, ntsb_engine.eng_serial_num=ntsb_engine_installation.eng_serial_num', 'Eng_Eng_Pair')
+                ops.filter_ra('Eng_EngInst_pair', 'eng.eng_mfrr=install.eng_mfrr, eng.eng_serial_num=install.eng_serial_num', 'Eng_Eng_Pair')
             )
         );
 
         ops.go(
-            ops.reduce_ra('Eng_Eng_Pair', 'aircr_id, eng_mfrr=ntsb_engine.mfrr, eng_serial_num=ntsb_engine.eng_serial_num', ops.allcols('Eng_Eng_Pair'), 'Eng_Aircr_id_Hr_Count')
+            ops.reduce_ra('Eng_Eng_Pair', 'aircr_id, eng_mfrr=eng.eng_mfrr, eng_serial_num=eng.eng_serial_num', ops.allcols('Eng_Eng_Pair'), 'Eng_Aircr_id_Hr_Count')
         );
-
-
 
 -- Second natural join
         ops.go(
-            ops.times_ra('Eng_Aircr_id_Hr_Count', 'ntsb_event', 'Eng_Event')
+            ops.times_ra('hour=Eng_Aircr_id_Hr_Count', 'evt=ntsb_event', 'Eng_Event')
         );
 
         ops.go(
             ops.filter_ra(
-                ops.filter_ra('Eng_Event', 'ntsb_event.aircr_id=Eng_Aircr_id_Hr_Count.aircr_id', 'Eng_Event_Pair')
+                ops.filter_ra('Eng_Event', 'evt.aircr_id=hour.aircr_id', 'Eng_Event_Pair')
             )
         );
 
         ops.go(
-            ops.reduce_ra('Eng_Event_Pair', 'aircr_id', 'evt_number, evt_injury_count, eng_tot_hr_count', 'Eng_Aircr_ID')
+            ops.reduce_ra('Eng_Event_Pair', 'aircr_id=evt.aircr_id', 'evt_number, evt_injury_count, eng_tot_hr_count', 'Eng_Aircr_ID')
         );
-
-
 
         ops.go(
             ops.group_ra('Eng_Aircr_ID', 'eng_tot_hr_count', 'total_injury_count = SUM(evt_injury_count', 'eng_hr_cnt_with_injury_cnt')
         );
-
-
-
 END;
 /
