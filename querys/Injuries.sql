0881f2361b323f8e8d043b2604c12c7858cfc4a4
Andrew King <king1031@umn.edu>
2017-12-15 03:13:57 -0600

    Another fix

--- querys/Injuries.sql
+++ querys/Injuries.sql
@@ -5,11 +5,11 @@ DECLARE
         );
 
         ops.go(
-            ops.filter_ra('Eng_EngInst_pair', 'eng.eng_mfrr=install.eng_mfrr, eng.eng_serial_num=install.eng_serial_num', 'Eng_Eng_Pair')
+            ops.filter_ra('Eng_EngInst_pair', 'eng_eng_mfrr=install_eng_mfrr, eng_eng_serial_num=install_eng_serial_num', 'Eng_Eng_Pair')
         );
 
         ops.go(
-            ops.reduce_ra('Eng_Eng_Pair', 'aircr_id, eng_mfrr=eng.eng_mfrr, eng_serial_num=eng.eng_serial_num', ops.allcols('Eng_Eng_Pair'), 'Eng_Aircr_id_Hr_Count')
+            ops.reduce_ra('Eng_Eng_Pair', 'aircr_id, eng_mfrr=eng_eng_mfrr, eng_serial_num=eng_eng_serial_num', ops.allcols('Eng_Eng_Pair'), 'Eng_Aircr_id_Hr_Count')
         );
 
 -- Second natural join
@@ -18,11 +18,11 @@ DECLARE
         );
 
         ops.go(
-            ops.filter_ra('Eng_Event', 'evt.aircr_id=hour.aircr_id', 'Eng_Event_Pair')
+            ops.filter_ra('Eng_Event', 'evt_aircr_id=hour_aircr_id', 'Eng_Event_Pair')
         );
 
         ops.go(
-            ops.reduce_ra('Eng_Event_Pair', 'aircr_id=evt.aircr_id', 'evt_number, evt_injury_count, eng_tot_hr_count', 'Eng_Aircr_ID')
+            ops.reduce_ra('Eng_Event_Pair', 'aircr_id=evt_aircr_id', 'evt_number, evt_injury_count, eng_tot_hr_count', 'Eng_Aircr_ID')
         );
 
         ops.go(
