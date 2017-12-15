9d917ea3758d1f09a6cda7df18285483c69a82bf
Curtice <huntck270@gmail.com>
2017-12-15 00:40:43 -0600

    data addition

--- sql/data/insert_data_flight_itinerary.sql
+++ sql/data/insert_data_flight_itinerary.sql
@@ -93,3 +93,15 @@ INSERT INTO ntsb_flight_itinerary (
 ) VALUES (
 
 );
+
+INSERT INTO ntsb_flight_itinerary (
+    flight_itin_id,
+    flight_departure_time,
+    flight_plan_activated_flag,
+    aircr_serial_num,
+    aircr_mfrr,
+    departure_airport_id,
+    destination_airport_id
+) VALUES (
+
+);
