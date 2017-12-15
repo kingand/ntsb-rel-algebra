dc22dbfb1c000eef32e81be527249bdb391c2290
Andrew King <king1031@umn.edu>
2017-12-15 01:24:54 -0600

    Adding quotes around varchar inserts

--- sql/data/insert_data_airport_information.sql
+++ sql/data/insert_data_airport_information.sql
@@ -3,5 +3,87 @@ INSERT INTO ntsb_airport (
     airport_name,
     airport_elevation
 ) VALUES (
+    "KDLH",
+    "",
+
+);
+
+INSERT INTO ntsb_airport (
+    airport_id,
+    airport_name,
+    airport_elevation
+) VALUES (
+    "KONA",
+    "",
+
+);
+
+INSERT INTO ntsb_airport (
+    airport_id,
+    airport_name,
+    airport_elevation
+) VALUES (
+    "PrivateAirField",
+    "",
+
+);
+
+INSERT INTO ntsb_airport (
+    airport_id,
+    airport_name,
+    airport_elevation
+) VALUES (
+    "Waren Muni",
+    "",
+
+);
+
+INSERT INTO ntsb_airport (
+    airport_id,
+    airport_name,
+    airport_elevation
+) VALUES (
+    "KFPR",
+    "",
+
+);
+
+INSERT INTO ntsb_airport (
+    airport_id,
+    airport_name,
+    airport_elevation
+) VALUES (
+    "PWT",
+    "",
+
+);
+
+INSERT INTO ntsb_airport (
+    airport_id,
+    airport_name,
+    airport_elevation
+) VALUES (
+    "KMHV",
+    "",
+
+);
+
+INSERT INTO ntsb_airport (
+    airport_id,
+    airport_name,
+    airport_elevation
+) VALUES (
+    "Private Areodome",
+    "",
+
+);
+
+INSERT INTO ntsb_airport (
+    airport_id,
+    airport_name,
+    airport_elevation
+) VALUES (
+    "Lemmon Valley",
+    "",
 
 );
