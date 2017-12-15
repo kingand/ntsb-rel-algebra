1ab75be3c67f96757235109bdb319d1e3e9cf19d
howlofthesun <holofthesun9@gmail.com>
2017-12-14 23:37:33 -0600

    fixed missing _ra on functions

--- querys/reciprocating.sql
+++ querys/reciprocating.sql
@@ -1,23 +1,23 @@
 DECLARE
 BEGIN
  ops.go(
-     ops.filter('ntsb_engine', 'eng_type=''Reciprocating''','Recip_Eng')
+     ops.filter_ra('ntsb_engine', 'eng_type=''Reciprocating''','Recip_Eng')
   );
 
   ops.go(
-      ops.project('Recip_Eng', 'eng_mfrr, eng_serial_num', 'ID_Recip_Eng' )
+      ops.project_ra('Recip_Eng', 'eng_mfrr, eng_serial_num', 'ID_Recip_Eng' )
   )
 
 ops.go(
-    ops.divide('ntsb_engine_installation', 'ID_Recip_Eng', 'ID_Recip_Eng_Inst')
+    ops.divide_ra('ntsb_engine_installation', 'ID_Recip_Eng', 'ID_Recip_Eng_Inst')
 )
 
 ops.go(
-    ops.mjoin('ntsb_aircraft', 'ID_Recip_Eng_Inst', 'aircr_id', 'aircr_id', 'Aircr_with_Recip')  )
+    ops.mjoin_ra('ntsb_aircraft', 'ID_Recip_Eng_Inst', 'aircr_id', 'aircr_id', 'Aircr_with_Recip')  )
 )
 
 ops.go(
-    ops.minus('ntsb_aircraft', 'Aircr_with_Recip', 'Aircr_without_Recip')
+    ops.minus_ra('ntsb_aircraft', 'Aircr_with_Recip', 'Aircr_without_Recip')
 )
 END;
 /
