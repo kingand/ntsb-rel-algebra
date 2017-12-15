5bbe244fd5e3b0021af1b5f3c7e641857586c2ec
Andrew King <king1031@umn.edu>
2017-12-15 01:10:18 -0600

    Fixing flight itinerary

--- sql/schema/create_table_flight_itinerary.sql
+++ sql/schema/create_table_flight_itinerary.sql
@@ -2,7 +2,7 @@ CREATE TABLE ntsb_flight_itinerary(
     flight_itin_id VARCHAR(30),
     flight_departure_time TIMESTAMP WITH TIME ZONE,
     flight_plan_activated_flag NUMBER(1),
-    aircr_id NUMBER(5) REFERENCES ntsb_airport(airport_id),
+    aircr_id NUMBER(5) REFERENCES ntsb_aircraft(aircr_id),
     departure_airport_id VARCHAR(10) REFERENCES ntsb_airport(airport_id),
     destination_airport_id VARCHAR(10) REFERENCES ntsb_airport(airport_id),
     PRIMARY KEY (flight_itin_id,aircr_id)
